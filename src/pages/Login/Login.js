a3cfa2ea11153035c635e52fa58b9eee6c3fc407
成飞 <chengfei@chengfeideMacBook-Pro.local>
2018-01-30 00:20:49 +0800

    代码优化

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -9,64 +9,64 @@ import styles from './index.less'
 const FormItem = Form.Item
 
 const Login = ({
-    dispatch,
-    form: { getFieldDecorator, validateFieldsAndScroll },
+  dispatch,
+  form: { getFieldDecorator, validateFieldsAndScroll },
 }) => {
-    function handleOk () {
-        validateFieldsAndScroll((errors, values) => {
-            if (errors) {
-                return
-            }
-            dispatch({ type: 'login/login', payload: values })
-        })
-    }
+  function handleOk () {
+    validateFieldsAndScroll((errors, values) => {
+      if (errors) {
+        return
+      }
+      dispatch({ type: 'login/login', payload: values })
+    })
+  }
 
-    return (
-      <div className={styles.form}>
-          <div className={styles.logo}>
-              <img alt="logo" src={Logo} />
-              <span>{config.appName}</span>
-            </div>
-          <form>
-              <FormItem hasFeedback>
-              {getFieldDecorator('username', {
-                        rules: [
-                            {
-                                required: true,
-                            },
-                        ],
-                    })(<Input onPressEnter={handleOk} placeholder="Username" />)}
-                </FormItem>
-              <FormItem hasFeedback>
-                  {getFieldDecorator('password', {
-                        rules: [
-                            {
-                                required: true,
-                            },
-                        ],
-                    })(<Input
-                      type="password"
-                      onPressEnter={handleOk}
-                      placeholder="Password"
-                    />)}
-                </FormItem>
-              <Row>
-                  <Button type="primary" onClick={handleOk}>
-                        Sign in
-                    </Button>
-                  <p>
-                      <span>Username：guest</span>
-                      <span>Password：guest</span>
-                    </p>
-                </Row>
-            </form>
-        </div>
-    )
+  return (
+    <div className={styles.form}>
+      <div className={styles.logo}>
+        <img alt="logo" src={Logo} />
+        <span>{config.appName}</span>
+      </div>
+      <form>
+        <FormItem hasFeedback>
+          {getFieldDecorator('username', {
+            rules: [
+              {
+                required: true,
+              },
+            ],
+          })(<Input onPressEnter={handleOk} placeholder="Username" />)}
+        </FormItem>
+        <FormItem hasFeedback>
+          {getFieldDecorator('password', {
+            rules: [
+              {
+                required: true,
+              },
+            ],
+          })(<Input
+            type="password"
+            onPressEnter={handleOk}
+            placeholder="Password"
+          />)}
+        </FormItem>
+        <Row>
+          <Button type="primary" onClick={handleOk}>
+            Sign in
+          </Button>
+          <p>
+            <span>Username：guest</span>
+            <span>Password：guest</span>
+          </p>
+        </Row>
+      </form>
+    </div>
+  )
 }
 
 Login.propTypes = {
-    form: PropTypes.object,
-    dispatch: PropTypes.func,
+  form: PropTypes.object,
+  dispatch: PropTypes.func,
 }
 
 export default connect(({ loading }) => ({ loading }))(Form.create()(Login))
