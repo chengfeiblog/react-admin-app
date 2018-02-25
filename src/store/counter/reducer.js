8b15b6db63a098ba30fabb9a6b7f91e1790ccb2b
成飞 <chengfei@chengfeideMacBook-Pro.local>
2018-02-25 17:24:03 +0800

    优化代码缩进

--- src/store/counter/reducer.js
+++ src/store/counter/reducer.js
@@ -12,17 +12,17 @@ const initState = {
    */
 export const countData = (state = initState, action = {}) => {
     switch (action.type) {
-    case types.INCREMENT:
-        return {
-            count: state.count + 1,
-        }
-    case types.DECREMENT:
-        return {
-            count: state.count - 1,
-        }
-    case types.RESET:
-        return { count: 0 }
-    default:
-        return state
+        case types.INCREMENT:
+            return {
+                count: state.count + 1,
+            }
+        case types.DECREMENT:
+            return {
+                count: state.count - 1,
+            }
+        case types.RESET:
+            return { count: 0 }
+        default:
+            return state
     }
 }
